1597a1717b858b00eff15bf03cec126615a9d0b8
Maxime ANANIVI <maximeananivi@192.168.1.106>
2025-11-13 21:25:15 +0000

    Corrected upload.middleware.js

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,5 +1,6 @@
 // multer is a middleware for handling multipart/form-data, which is primarily used for file uploads
 
+// upload.middleware.js
 import multer from "multer";
 
 const storage = multer.memoryStorage();
@@ -15,7 +16,10 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  limits: { 
+    fileSize: 5 * 1024 * 1024, // 5MB limit
+    files: 1 // Maximum 1 file
+  },
 });
 
 export default upload;
